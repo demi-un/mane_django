2d4b24807018b795390902cd9d65e9c3e6e78fa2
Guzhov Roman <guzovvrom277@gmail.com>
2025-11-06 00:57:57 +0500

    asdf

--- mane01.py
+++ mane01.py
@@ -1,9 +1,9 @@
-import urllib.parse
-
-string = [
-    "Как стать backend разработчиком", 
-    "Привет!"
-]
-
-print(urllib.parse.quote(string[1]))
-print(urllib.parse.unquote(string[1]))
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
