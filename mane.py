f9c7f0995200b3b64f5af3b59a37a25246ea098c
Guzhov Roman <guzovvrom277@gmail.com>
2025-11-05 22:01:58 +0500

    initial commit

---
+++ mane.py
@@ -0,0 +1,5 @@
+print("qwertyuikjhgyjdwnfhsjfkdmefnjghjikwmenjfghlijwdekmjnfbhfgjfkdmengjb,gfkdmfvgbgfkdmfn gbj,gfkmgjbgfkmg bjgfk")
+
+
+# cmd + d - следущее выделение
+# cmd + shift + L - выделить все похожие слова
