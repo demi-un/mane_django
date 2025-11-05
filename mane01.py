88b26ead4876856699ece3becc500ccfaec7b8ee
Guzhov Roman <guzovvrom277@gmail.com>
2025-11-05 22:45:01 +0500

    bghj

--- mane01.py
+++ mane01.py
@@ -1,8 +1,9 @@
-print("Hello, world")
-print("Hello, world")
-print("Hello, world")
-print("Hello, world")
-print("Hello, world")
-print("Hello, world")
-print("Hello, world")
-print("Hello, world")
+import urllib.parse
+
+string = [
+    "Как стать backend разработчиком", 
+    "Привет!"
+]
+
+print(urllib.parse.quote(string[1]))
+print(urllib.parse.unquote(string[1]))
